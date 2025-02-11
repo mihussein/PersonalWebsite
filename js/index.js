c57accfdea9944953492f9a7e6ffafd588021f87
Hussein <herrhussein@proton.me>
2025-02-11 10:17:50 +0330

    new header

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 'use strict'
-let navigationBar = document.querySelector('.navigation_bar');
-let navigationBarIcon = document.querySelector('.navigation_bar_icon');
-let navigationBarList = document.querySelector('.navigation_bar_list');
-let computedStyle = window.getComputedStyle(navigationBarList);
-console.log(computedStyle.left);
-navigationBar.addEventListener('click', () => {
-    navigationBarList.style.display = navigationBarList.style.display === 'none' ? 'block' : 'none';
-    navigationBarList.style.left = navigationBarList.style.left === '50px' ? '-20px' : '50px';
-});
+// let sidebar = document.querySelector('.sidebar');
+// let sidebarIcon = document.querySelector('.sidebar_icon');
+// let sidebarList = document.querySelector('.sidebar_list');
+// let computedStyle = window.getComputedStyle(sidebarList);
+// let sidebarIconContainer = document.querySelector('.sidebar_icon_container');
+
+// sidebarIconContainer.addEventListener('click', () => {
+//     sidebarList.style.display = sidebarList.style.display === 'none' ? 'block' : 'none';
+//     sidebarList.style.left = sidebarList.style.left === '50px' ? '-20px' : '50px';
+//     // sidebarList.style.width = computedStyle.width === '60px' ? '0px' : '60px';
+//     sidebarList.style.width = "60px";
+// });
+
+function toggleSidebar() {
+    document.querySelector(".sidebar").classList.toggle("active");
+  }
